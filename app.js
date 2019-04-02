393f33cceba18e40b4505a6afd7abecae0430410
Tony Owl <antoniovaldenisio@gmail.com>
2019-04-02 18:10:41 -0300

    Display Data and Wrap Up

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ function searchWeather() {
         return alert('Please enter a city name');
     };    
 
+    ELEMENTS.ELEMENT_LOADING_TEXT.style.display = 'block';
+    ELEMENTS.ELEMENT_WEATHER_BOX.style.display = 'none';
+
     const URL = `http://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&units=metric&APPID=${APP_ID}`;
 
     Http.fetchData(URL)
@@ -19,6 +22,16 @@ function searchWeather() {
             const WEATHER_DATA = new WeatherData(CITY_NAME, responseData.weather[0].description.toUpperCase());
             const WEATHER_PROXY = new Proxy(WEATHER_DATA, WEATHER_PROXY_HANDLER);
             WEATHER_PROXY.temperature = responseData.main.temp;
+            updateWeather(WEATHER_PROXY);
         })
         .catch(error => alert(error));
 };
+
+function updateWeather(weatherData) {
+    ELEMENTS.ELEMENT_WEATHER_CITY.textContent = weatherData.cityName;
+    ELEMENTS.ELEMENT_WEATHER_DESCRIPTION.textContent = weatherData.description;
+    ELEMENTS.ELEMENT_WEATHER_TEMPERATURE.textContent = weatherData.temperature;
+
+    ELEMENTS.ELEMENT_LOADING_TEXT.style.display = 'none';
+    ELEMENTS.ELEMENT_WEATHER_BOX.style.display = 'block';
+}
