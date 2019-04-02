2239b9b64cb1d2f04a3d6be8ddd9f05dd1b64dc0
Tony Owl <antoniovaldenisio@gmail.com>
2019-04-02 17:47:35 -0300

    Sending the HTTP Request

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 import * as ELEMENTS from 'elements.js';
+import { Http } from 'http.js';
+
+const APP_ID = 'cc11693e309be73d09b485264ca4ab5c';
 
 ELEMENTS.ELEMENT_SEARCH_BUTTON.addEventListener('click', searchWeather);
 
@@ -6,6 +9,13 @@ function searchWeather() {
     const CITY_NAME = ELEMENTS.ELEMENT_SEARCHED_CITY.value.trim();
     if (CITY_NAME.length == 0) {
         return alert('Please enter a city name');
-    };
-    alert(CITY_NAME);
+    };    
+
+    const URL = `http://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&units=metric&APPID=${APP_ID}`;
+
+    Http.fetchData(URL)
+        .then(responseData => {
+            console.log(responseData);
+        })
+        .catch(error => alert(error));
 };
